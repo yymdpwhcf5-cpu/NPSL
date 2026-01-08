fb9a3b94c5cfbd0e7abc6c61176dd70a797a0aae
yymdpwhcf5-cpu <yymdpwhcf5@privaterelay.appleid.com>
2026-01-08 16:59:06 -0500

    Update server.js
    Fix server.js app definition

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("NPSL server is running ✅");
+});
+
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`NPSL server listening on port ${PORT}`);
 });
